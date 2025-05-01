ab4159b29119e87f316bae414dcfb4ac3cead1ad
ferraiolo80 <ferraiolo80@hotmail.it>
2025-05-01 22:38:19 +0200

    Update app.js

--- app.js
+++ app.js
@@ -335,6 +335,8 @@ async function applyFilters() {
   const category = categoryFilter.value;
   const tempMin = tempMinFilter.value ? Number(tempMinFilter.value) : null;
   const tempMax = tempMaxFilter.value ? Number(tempMaxFilter.value) : null;
+  console.log("Filtro ricerca:", searchTerm); // <-- Aggiungi questi log
+  console.log("Filtro categoria:", category);
   console.log("Filtro temperatura min:", tempMin);
   console.log("Filtro temperatura max:", tempMax);
   const searchResultDiv = document.getElementById('garden-container');
