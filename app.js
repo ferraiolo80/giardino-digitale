90338670568b852b2f05df24ec0fdfe6a17c9fc9
ferraiolo80 <ferraiolo80@hotmail.it>
2025-05-02 12:15:23 +0200

    Update app.js

--- app.js
+++ app.js
@@ -201,38 +201,6 @@ async function savePlantToFirebase(newPlant) {
   }
 }
 // === FUNZIONI DI RENDERING ===
-function renderPlants(plantArray) {
-  gardenContainer.innerHTML = "";
-  plantArray.forEach((plant) => {
-    const plantCard = document.createElement("div");
-    plantCard.className = "plant-card";
-
-    const isInMyGarden = myGarden.includes(plant.id);
-    const buttonText = isInMyGarden ? "Già nel giardino" : "Aggiungi al mio giardino";
-    const buttonDisabled = isInMyGarden ? "disabled" : "";
-
-    plantCard.innerHTML = `
-      <h3>${plant.name}</h3>
-      <p><strong><i class="fas fa-sun" style="margin-right: 5px;"></i> Luce:</strong> ${plant.sunlight}</p>
-      <p><strong><i class="fas fa-tint" style="margin-right: 5px;"></i> Acqua:</strong> ${plant.watering}</p>
-      <p><strong><i class="fas fa-thermometer-low" style="margin-left: 5px; font-size: 1em;"></i> Temperatura ideale min:</strong> ${plant.tempMin}°C</p>
-      <p><strong><i class="fas fa-thermometer-high" style="margin-right: 5px; font-size: 1em;"></i> Temperatura ideale max:</strong> ${plant.tempMax}°C</p>
-      ${plant.description ? `<p><strong>Descrizione:</strong> ${plant.description}</p>` : ""}
-      ${plant.image ? `<img src="${plant.image}" alt="${plant.name}" width="100">` : ""}
-      <button class="add-to-garden-button" data-plant-name="${plant.name}" ${buttonDisabled}>${buttonText}</button>
-    `;
-
-    gardenContainer.appendChild(plantCard);
-
-    const addButton = plantCard.querySelector('.add-to-garden-button');
-    if (!isInMyGarden) {
-      addButton.addEventListener('click', () => {
-        const plantNameToAdd = addButton.dataset.plantName;
-        addToMyGarden(plantNameToAdd);
-      });
-    }
-  });
-}
   
 async function renderMyGarden(gardenArray) {
   const gardenIds = gardenArray || myGarden; // Ora 'myGarden' contiene solo ID
