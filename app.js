92c29c05ceb88cc7c2312c2d815e6f6ca63e63e8
ferraiolo80 <ferraiolo80@hotmail.it>
2025-05-06 01:50:40 +0200

    Update app.js

--- app.js
+++ app.js
@@ -201,7 +201,7 @@ function renderPlants(plantArray) {
         div.className = "plant-card";
         div.innerHTML = `
             <img src="${image}" alt="${plant.name}" class="plant-icon">
-            <h4><span class="math-inline">\{plant\.name\}</h4\>
+            <h4>${plant.name}</h4>
             <p>Luce: ${plant.sunlight}</p>
             <p>Acqua: ${plant.watering}</p>
             <p>Temperatura ideale min: ${plant.tempMin}°C</p>
