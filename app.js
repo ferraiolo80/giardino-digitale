6f6c4dd26714871d866df9196408e7c04c6c9406
ferraiolo80 <ferraiolo80@hotmail.it>
2025-05-09 23:59:04 +0200

    Update app.js

--- app.js
+++ app.js
@@ -64,6 +64,7 @@ document.addEventListener('DOMContentLoaded', async () => {
     const validGarden = []; // Nuovo array per contenere solo ID validi
 
     for (const plantId of safeGarden) {
+        console.log("INIZIO CICLO RENDERMYGARDEN CON ID:", plantId); // NUOVO LOG
         console.log("ID della pianta prima della query Firebase:", plantId); // Aggiungi questo log
         console.log("ID della pianta prima del try:", plantId, typeof plantId); // Aggiungi questo lo
         console.log("Tentativo di recuperare la pianta con ID:", plantId); 
