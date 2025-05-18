e1f29079ed83cecf2cac30e0d3ca93a8d8fcfca2
ferraiolo80 <ferraiolo80@hotmail.it>
2025-05-18 17:33:33 +0200

    Update app.js

--- app.js
+++ app.js
@@ -118,8 +118,8 @@ async function renderPlants(plantArray) {
             <h4>${plant.name}</h4>
             <p><i class="fas fa-sun"></i> Luce: ${plant.sunlight}</p>
             <p><i class="fas fa-tint"></i> Acqua: ${plant.watering}</p>
-            <p><i class="fas fa-thermometer-half"></i> Temperatura ideale min: ${plant.tempMin}°C</p>
-            <p><i class="fas fa-thermometer-half"></i> Temperatura ideale max: ${plant.tempMax}°C</p>
+            <p><i class="fas fa-thermometer-half"></i> Temp ideale min: ${plant.tempMin}°C</p>
+            <p><i class="fas fa-thermometer-half"></i> Temp ideale max: ${plant.tempMax}°C</p>
             <p>Categoria: ${plant.category}</p>
             ${user ?
                 myGardenFB.includes(plant.id) ?
