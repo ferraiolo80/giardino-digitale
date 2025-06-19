c900336684b1e25a5334c9b2143a1e7b5af42ba1
ferraiolo80 <ferraiolo80@hotmail.it>
2025-06-20 01:51:17 +0200

    Update app.js

--- app.js
+++ app.js
@@ -380,6 +380,7 @@ async function editPlant(plantId) {
     if (plantToEdit) {
         plantNameInput.value = plantToEdit.name || '';
         plantCategorySelect.value = plantToEdit.category || '';
+        plantFormElement.querySelector('#plantSunExposure').value = plant.sunExposure || '';
         plantDescriptionTextarea.value = plantToEdit.description || '';
         plantTempMinInput.value = plantToEdit.tempMin || '';
         plantTempMaxInput.value = plantToEdit.tempMax || '';
